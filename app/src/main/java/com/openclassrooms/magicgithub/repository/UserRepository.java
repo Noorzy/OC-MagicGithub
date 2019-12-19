1e988cd3280e7f5c678ccc6f7871c6ef295a25e8
Noorzy <Noorzy@users.noreply.github.com>
2019-12-19 16:43:52 +0100

    .
      .

---
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -0,0 +1,49 @@
+package com.openclassrooms.magicgithub.repository;
+
+import com.openclassrooms.magicgithub.api.ApiService;
+import com.openclassrooms.magicgithub.model.User;
+import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS;
+
+import java.util.ArrayList;
+
+
+import java.util.List;
+
+public class UserRepository {
+
+    private final ApiService apiService; // TODO: A utiliser
+
+    public UserRepository(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+      private List<User> WorkArray = new ArrayList<>(FAKE_USERS);
+
+
+    public List<User> getUsers() {
+        // TODO: A modifier
+
+
+
+
+
+        return WorkArray;
+
+
+
+
+    }
+
+    public void generateRandomUser() {
+        // TODO: A modifier
+        WorkArray.add(User.random());
+    }
+
+    public void deleteUser(User user) {
+        // TODO: A modifier
+
+         WorkArray.remove(user);
+
+
+    }
+}
