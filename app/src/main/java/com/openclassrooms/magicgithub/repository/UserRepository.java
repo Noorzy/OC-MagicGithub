e77f12f935686b6e97dfb13f5862f77a7d4d1b5a
Noorzy <Noorzy@users.noreply.github.com>
2019-12-23 11:04:53 +0100

    .
    .

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -12,42 +12,24 @@ import java.util.List;
 
 public class UserRepository {
 
-    private final ApiService apiService; // TODO: A utiliser
+    private final ApiService apiService;
 
     public UserRepository(ApiService apiService) {
         this.apiService = apiService;
     }
 
-      //private List<User> WorkArray = new ArrayList<>(FAKE_USERS);
+
 
 
     public List<User> getUsers() {
-        // TODO: A modifier
-
-
-
-
-
-        //return WorkArray;
         return apiService.getUsers();
-
-
-
-
     }
 
     public void generateRandomUser() {
-        // TODO: A modifier
-       // WorkArray.add(User.random());
         apiService.generateRandomUser();
     }
 
     public void deleteUser(User user) {
-        // TODO: A modifier
-
-        // WorkArray.remove(user);
         apiService.deleteUser(user);
-
-
     }
 }
