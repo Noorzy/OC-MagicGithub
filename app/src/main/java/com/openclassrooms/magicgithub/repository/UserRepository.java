d94b9eabe10b49ea742f550d0717832ec0350bd5
Noorzy <Noorzy@users.noreply.github.com>
2019-12-23 11:27:49 +0100

    .
    .

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -2,11 +2,6 @@ package com.openclassrooms.magicgithub.repository;
 
 import com.openclassrooms.magicgithub.api.ApiService;
 import com.openclassrooms.magicgithub.model.User;
-import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS;
-import com.openclassrooms.magicgithub.api.FakeApiService;
-
-import java.util.ArrayList;
-
 
 import java.util.List;
 
